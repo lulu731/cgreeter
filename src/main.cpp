2df3b77fcb5ae8a8a03a84740951c75584aa3c7c
lulu731 <lulu731lulu@hotmail.fr>
2023-05-30 13:37:14 +0200

    Using native_byte_order files in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "native_byte_order.hpp"
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
@@ -15,11 +16,13 @@ int main(int argc, char* argv[])
   try
   {
     using namespace std; // For strlen.
-    std::cout << "Enter message: ";
-    const char *len_bytes = ".\0\0\0";
     const char *request = "{\"type\": \"create_session\", \"username\": \"lulu\"}";
     size_t request_length = strlen(request);
     std::cout << request_length << std::endl;
+
+    char len_bytes[4];
+    ToNativeByteOrder(request_length, len_bytes);
+    std::cout << len_bytes[0] << std::endl;
 
     boost::asio::io_context io_context;
 
@@ -31,22 +34,27 @@ int main(int argc, char* argv[])
     s.connect(stream_protocol::endpoint(buf));
     std::cout << s.is_open() << std::endl;
 
-    size_t sent = boost::asio::write(s, boost::asio::buffer(len_bytes, 46),
+    size_t sent = boost::asio::write(s, boost::asio::buffer(len_bytes, request_length),
       boost::asio::transfer_exactly(4));
-    sent = boost::asio::write(s, boost::asio::buffer(request, 46),
-      boost::asio::transfer_exactly(46));
+    sent = boost::asio::write(s, boost::asio::buffer(request, request_length),
+      boost::asio::transfer_exactly(request_length));
     std::cout << request << std::endl;
 
     std::cout << sent << std::endl;
 
-    char reply[max_length];
+    char reply_bytes[4];
     std::cout << "in Reply" << std::endl;
     size_t reply_length = boost::asio::read(s,
-        boost::asio::buffer(reply, 4), boost::asio::transfer_exactly(4));
+        boost::asio::buffer(reply_bytes, 4), boost::asio::transfer_exactly(4));
     std::cout << "reply_length1 = " <<reply_length << std::endl;
 
+    size_t reply_size;
+    FmNativeByteOrder(reply_bytes, reply_size);
+    std::cout << reply_size << std::endl;
+
+    char reply[reply_size];
     reply_length = boost::asio::read(s,
-        boost::asio::buffer(reply, 80), boost::asio::transfer_exactly(80));
+        boost::asio::buffer(reply, reply_size), boost::asio::transfer_exactly(reply_size));
     std::cout << "reply_length2 = " <<reply_length << std::endl;
 
 
